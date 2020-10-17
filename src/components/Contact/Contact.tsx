e272c8c5603e582f32fee6b9b3cc08838850da30
shpetimsedolli <shpeti101@gmail.com>
2020-10-17 17:25:18 +0200

    added contact componen with scss style

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -2,11 +2,22 @@ import React from 'react'
 
 import './Contact.scss'
 
+import contactbanner from '../../assets/images/contact-banner.jpg'
+
 export const Contact = () => {
     return (
-        <div className="Contact">
-            <h3>HELLO HELLO
-            </h3>
+        <div className="container">
+            <div className="contact">
+                <div className="contact__thumb">
+                    <img src={contactbanner} alt="" />
+                </div>
+                <div className="contact__content">
+                    <h4>Subscribe to our newsletter and
+                        receive exclusive offers every week</h4>
+                    <input type="text" placeholder="Enter your email" />
+                    <button>Subscribe</button>
+                </div>
+            </div>
         </div>
     )
 }
