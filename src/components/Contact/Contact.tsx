5868b8c9fceaf9b916562b3be2f43f76bb5eeb56
shpetimsedolli <shpeti101@gmail.com>
2020-10-17 16:59:00 +0200

    added arrival section

---
+++ src/components/Contact/Contact.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import './Contact.scss'
+
+export const Contact = () => {
+    return (
+        <div className="Contact">
+            <h3>HELLO HELLO
+            </h3>
+        </div>
+    )
+}
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Slider from "react-slick";
 
 //Components
+import { Contact } from '../../components/Contact/Contact';
 
 //styles
 import './Home.scss'
@@ -9,6 +10,10 @@ import './Home.scss'
 import banner1 from '../../assets/images/banner-1.jpg'
 import banner2 from '../../assets/images/banner-2.jpg'
 import banner3 from '../../assets/images/banner-3.jpg'
+import arrival1 from '../../assets/images/arrival1.jpg'
+import arrival2 from '../../assets/images/arrival2.jpg'
+
+
 
 const settingsBanner = {
     dots: false,
@@ -50,6 +55,35 @@ export const Home = () => {
                     </div>
                 </Slider>
             </section>
-        </div>
+            <section className="block_section arrivals">
+                <div className="container">
+                    <div className="row has_gutter">
+                        <div className="column-6">
+                            <div className="box">
+                                <div className="box__thumb">
+                                    <img src={arrival1} alt="" />
+                                </div>
+                                <div className="box__content">
+                                    <h3>New arrivals are now in!</h3>
+                                    <button>Show Collection</button>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="column-6">
+                            <div className="box">
+                                <div className="box__thumb">
+                                    <img src={arrival2} alt="" />
+                                </div>
+                                <div className="box__content">
+                                    <h3>Basic t-shirts $29,99</h3>
+                                    <button>More Details</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <Contact />
+        </div >
     )
 }
